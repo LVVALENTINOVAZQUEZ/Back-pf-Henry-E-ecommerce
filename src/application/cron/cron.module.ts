2b3fe03f059d9995cf061a7b383c8bdcf17d1187
valentino <valen3601@outlook.com>
2025-12-03 13:52:43 -0300

    Subiendo implementación de crons

--- src/application/cron/cron.module.ts
+++ src/application/cron/cron.module.ts
@@ -3,11 +3,15 @@ import { CronService } from './cron.service';
 import { BookingsService } from '../bookings/bookings.service';
 import { MailerService } from '../mailer/mailer.service';
 import { ScheduleModule } from '@nestjs/schedule';
+import { CouponsModule } from '../coupons/coupons.module';
 
 
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [
+    ScheduleModule.forRoot(),
+    CouponsModule,
+  ],
   providers: [CronService, BookingsService, MailerService],
 })
 export class CronModule {}
